3259bdb82a37dbd5714ac924f5024c53f4d4bae6
Olga <olgarciaq11@gmail.com>
2025-05-28 13:32:31 +0200

    Terminado el menu

--- proyecto_equipo/src/main/java/com/proyecto/gui/Main.java
+++ proyecto_equipo/src/main/java/com/proyecto/gui/Main.java
@@ -1,7 +1,103 @@
 package com.proyecto.gui;
 
+import java.util.List;
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.proyecto.dao.PersonaDAO;
+import com.proyecto.model.Persona;
+import com.proyecto.util.HibernateUtil;
+
 public class Main {
+
+    static void mostrarMenu() {
+        System.out.println("Seleccione opción:");
+        System.out.println("1. Insertar persona");
+        System.out.println("2. Borrar persona");
+        System.out.println("0. Salir");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        int opcion = 12;
+        do {
+
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+
+            PersonaDAO personaDAO = new PersonaDAO();
+
+            try {
+                Scanner s = new Scanner(System.in);
+				mostrarMenu();
+	            opcion = s.nextInt();
+	            s.nextLine();
+	            
+	            switch(opcion) {
+
+                    //INSERTAR PERSONA:
+	            
+	            	case 1:
+                    
+                    System.out.println("Pon un nombre");
+                    String nombre = s.next();
+
+                    System.out.println("Pon una edad");
+                    int edad = s.nextInt();
+
+                    Persona p = new Persona(nombre, edad);
+
+                    personaDAO.insertPersona(session, p);
+
+                    List<Persona> personas = personaDAO.selectAllPersona(session);
+
+                    for (Persona pe : personas) {
+                        System.out.println(pe.getIdPersona + " " + pe.getNombre + " " + pe.getEdad); 
+                    }
+	            		
+	            	break;
+	            	
+	            	case 2:
+	            		
+	            		//BORRAR UN ESTUDIANTE:
+
+                        System.out.println("Pon un id");
+                        int id = s.nextInt();
+
+                        personaDAO.deletePersona(session, id);
+
+                        personas = personaDAO.selectAllPersonas(session);
+
+                        for (Persona pe : personas) {
+                            System.out.println(pe.getIdPersona + " " + pe.getNombre + " " + pe.getEdad); 
+                        }
+
+	            		
+	            	break;
+	            	
+	            	case 0:
+		                    System.out.println("Fin del programa");
+		            break;
+		            
+		            default:
+		                    System.out.println("Opción no válida");
+	            		
+	            }
+				transaction.commit();
+                session.clear();
+			}catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            } finally {
+                session.close();
+            }
+            
+
+        } while (opcion != 0);
+
+
     }
 }
